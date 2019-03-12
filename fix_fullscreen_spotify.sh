abd36e21be5da6888cb42a14cdcc5dc6ff515acf
Rodrigo Garcês <rmgarces1@gmail.com>
2019-03-12 15:09:42 -0300

    Updated fix spotify fullscreenn
    Now only reduce spotify screen, without logout user

--- fix_fullscreen_spotify.sh
+++ fix_fullscreen_spotify.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-rm .config/spotify/window_position.prefs
+sed -i '/app.window.position.width=/c\app.window.position.width=800' .config/spotify/prefs
+sed -i '/app.window.position.height=/c\app.window.position.height=600' .config/spotify/prefs
